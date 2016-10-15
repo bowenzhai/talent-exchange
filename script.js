9fa4f51290f31be9cb1b5e3b1782481d6d7d62e4
Bowen Zhai <kevinzhai97@gmail.com>
2016-10-15 11:30:24 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.banner').slick({
+	arrows: true
+  });
+});
+
+var loginBtn = document.getElementsByTagName("nav")[0].children[3];
+var loginWindow =  document.getElementById("login_Window");3
+var bg = document.getElementsByClassName("transparent-bg")[0];
+loginBtn.onclick = function() {
+	/*loginWindow.style.display = "block";
+	loginWindow.style.opacity = "1";*/
+	//$('#login_Window').animate({opacity: 1},100);
+	bg.style.display = "block";
+	bg.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+	bg.style.opacity = "1";
+}
